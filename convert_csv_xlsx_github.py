ed7d4c2a1761f7006b90a9b4f473d0063db31d70
Ifeoma Melissa Collins <ifeomacollins@users.noreply.github.com>
2017-12-19 15:35:46 -0500

    Convert CSV to XLSX
    Convert CSV files in a folder to XLSX files in a different folder

---
+++ convert_csv_xlsx_github.py
@@ -0,0 +1,43 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:  convert csv files in folder to xlsx and save to another folder
+#
+# Author:      Ifeoma.Collins
+#
+# Created:     19-12-2017
+# Copyright:   (c) Ifeoma.Collins 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+#https://stackoverflow.com/questions/12976378/openpyxl-convert-csv-to-excel
+#https://stackoverflow.com/questions/3964681/find-all-files-in-a-directory-with-extension-txt-in-python
+
+
+import csv, openpyxl, os, glob
+
+
+#list of csv files in directory
+csv_folder = r"C:\csvs" + "\\"
+
+#save xls in this folder
+xlxs_folder =  r"C:\xlsx" + "\\"
+os.chdir(csv_folder)
+
+wb = openpyxl.Workbook()
+ws = wb.active
+
+for file in glob.glob("*.csv"):
+    filename = csv_folder+file
+    print filename
+    f = open(filename)
+    reader = csv.reader(f,delimiter=":")
+
+    for row in reader:
+        ws.append(row)
+    f.close()
+
+    wb.save(xlxs_folder+file[:-4]+".xlsx")
+
+
+print "done"
